43d7cfe7affbadbe02a36fba997b2e3e103c48e3
vmuthyala <vmuthyala@GBWK-INA034036Z.galaxy.lan>
2019-12-20 18:49:44 +0530

    spring core 1

---
+++ src/main/java/com/mvn/spring/springcore1/Test.java
@@ -0,0 +1,16 @@
+package com.mvn.spring.springcore1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+		Employee emp = (Employee)ctx.getBean("emp");
+		System.out.println("Id:" + emp.getId());
+		System.out.println("Name:" + emp.getName());
+		
+	}
+
+}
