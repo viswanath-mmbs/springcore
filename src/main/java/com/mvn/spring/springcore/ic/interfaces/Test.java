077bf759b913caf1bd0d7879ec602781752da90b
vmuthyala <vmuthyala@GBWK-INA034036Z.galaxy.lan>
2019-12-20 19:51:13 +0530

    Inversion of control life cycle methods using interface configuraton

---
+++ src/main/java/com/mvn/spring/springcore/ic/interfaces/Test.java
@@ -0,0 +1,17 @@
+package com.mvn.spring.springcore.ic.interfaces;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("com/mvn/spring/springcore/ic/interfaces/config.xml");
+		Patient p = (Patient)ctx.getBean("patient");
+		System.out.println(p);
+		
+		ctx.registerShutdownHook(); // need to invoke to tell spring container to invoke destroy method
+	}
+
+}
