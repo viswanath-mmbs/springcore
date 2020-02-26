c27356f677298dcd9e44ef18814e18150f5f9d27
vmuthyala <vmuthyala@GBWK-INA034036Z.galaxy.lan>
2020-02-26 17:29:37 +0530

    loose coupling using runtime poly morphisam

---
+++ src/main/java/com/mvn/spring/springcore/session13/injectInterface/OrderDAOImpl2.java
@@ -0,0 +1,12 @@
+package com.mvn.spring.springcore.session13.injectInterface;
+
+public class OrderDAOImpl2 implements OrderDAO {
+
+	@Override
+	public void createOrder() {
+
+		System.out.println("Inside OrderDAOImpl2 Create Order");
+
+	}
+
+}
